6efe6178a59c8d35082489986d95aff12df59b66
bchartier <benjamin.chartier@cegetel.net>
2014-08-05 20:09:17 +0200

    Compléments pour suppression de petits tronçons (fix #9)

--- post_imposm/clean_tiny_geometries.sql
+++ post_imposm/clean_tiny_geometries.sql
@@ -1,9 +1,13 @@
 -- post-traitements sur la base OSM après exécution de imposm
--- suppression des tronçons de routes généralisés de longueur nulle
+-- suppression des tronçons de voies généralisés de longueur nulle
 
 begin;
 
-delete from osm_motorways_gen0 where st_length(geometry) = 0;
-delete from osm_motorways_gen1 where st_length(geometry) = 0;
+DELETE FROM osm_motorways_gen0 WHERE st_length(geometry) = 0;
+DELETE FROM osm_motorways_gen1 WHERE st_length(geometry) = 0;
+DELETE FROM osm_mainroads_gen0 WHERE st_length(geometry) = 0;
+DELETE FROM osm_mainroads_gen1 WHERE st_length(geometry) = 0;
+DELETE FROM osm_railways_gen0 WHERE st_length(geometry) = 0;
+DELETE FROM osm_railways_gen1 WHERE st_length(geometry) = 0;
 
 commit;
