c97c8e5b9558267e50d937c10947405651afabd8
bchartier <benjamin.chartier@cegetel.net>
2014-08-12 06:31:30 +0200

    Filtre des relations pour les itinéraires routiers

--- post_imposm/roads_n_railways/roads_relation_ref_int_ref.py
+++ post_imposm/roads_n_railways/roads_relation_ref_int_ref.py
@@ -8,11 +8,11 @@ class HighwayCounter(object):
   nb_relations = 0
   nb_route_relations = 0
 
-  def ways(self, ways):
-    # callback method for ways
-    for osmid, tags, refs in ways:
-      if 'highway' in tags:
-        self.nb_highways += 1
+  # def ways(self, ways):
+  #   # callback method for ways
+  #   for osmid, tags, refs in ways:
+  #     if 'highway' in tags:
+  #       self.nb_highways += 1
 
   def relations(self, relations):
     # callback method for relations
@@ -21,13 +21,17 @@ class HighwayCounter(object):
       self.nb_relations += 1
       if ('type' in tags) and (tags['type'] == 'route') and ('route' in tags) and (tags['route'] == 'road'):
       # if ('ref' in tags):
-        print tags
+        print osmid, tags.get('name'), tags.get('ref'), tags.get('network'), len(members)
+        # if tags.get('ref') == "E 46":
+        #   print tags
         self.nb_route_relations += 1
 
 # instantiate counter and parser and start parsing
 counter = HighwayCounter()
-p = OSMParser(concurrency=4, ways_callback=counter.ways, relations_callback=counter.relations)
+# p = OSMParser(concurrency=4, ways_callback=counter.ways, relations_callback=counter.relations)
+p = OSMParser(concurrency=4, relations_callback=counter.relations)
 p.parse('/home/benjamin/imposm/pbf/champagne-ardenne-latest.osm.pbf')
+
 
 # done
 print counter.nb_highways
